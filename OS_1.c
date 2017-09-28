517fd8bdea6dcc1e2a7bdff453d603c5f56c655c
BobrG <bobrovskihg@gmail.com>
2017-09-29 00:19:18 +0300

    edited handler

--- OS_1.c
+++ OS_1.c
@@ -11,18 +11,22 @@
 #include <time.h>
 #include <unistd.h>
 #include <setjmp.h>
+#include <sys/signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 
 sigjmp_buf obl;
-int count = 0;
+volatile sig_atomic_t count = 0;
+static char* args[100] = {NULL};
 
-int get_args(char* args, char* first_arg, char** dest){
+// functon get_args 
+// parsing a string on words with spaces as separators.
+void get_args(char* args, char* first_arg, char** dest){
     int n, i, j, l;
 
-
+    l = 0;
     // start from first word;
     for (j = 0; j < strlen(args) - 1; ++j){
         if (args[j] != ' '){
@@ -58,41 +62,55 @@ int get_args(char* args, char* first_arg, char** dest){
     }
 
     
-    dest[n] = NULL; 
-    n++;
-    return n;
-    
 }
 
-void handle(){
+//not ready yet
+void handle(int sig){
+    int i;
+    FILE* file;
+    char line[256];
     count ++;
-    signal (SIGINT, handle);
     if (count >= 3){
-        printf("show file info");
+        for (i=1; args[i] != NULL; ++i){
+            if (args[i][0] != '-'){
+                printf("\nFile %d:\n %s\n", i+1, args[i]);
+                file = fopen(args[i], "r");
+                while (fgets(line, sizeof(line), file)){
+                    printf("%s\n", line);
+                }
+                fclose(file);
+            }
+        }
         exit(count);
     }
-    siglongjmp (obl, 1);    /* возвращение на последний setjmp */
+    siglongjmp (obl, 1);    /* returns to last setjmp */
 }
 
 int main(int argc, char *argv[]){
    
     pid_t pid;
-    int i, j, count;
+    struct sigaction new_act, old_act;
+    int i, j, amount;
     int mypipe[2];
     int pipe_ret;
     char buff_p[100], buff_c[100];
-    char** args;
     char* first_arg;
-
 
     pipe_ret = pipe(mypipe);
 
-    signal(SIGINT, handle);
 
-    sigsetjmp(obl, 1);
+    // sa.sa_flags = SA_SIGINFO;
+    // new_act.sa_sigaction = handle;
+    new_act.sa_handler = handle;
+
     if (pipe_ret == -1){
-        perror("Creating a pipe ERROR");
+        perror("Creating a pipe ERROR\n");
         exit(1);
+    }
+    
+    if (sigaction(SIGINT, &new_act, NULL) < 0) {
+        perror ("Sigaction ERROR\n");
+        exit(2);
     }
 
     pid = fork();
@@ -102,31 +120,36 @@ int main(int argc, char *argv[]){
     }
     else if (pid == 0){
         //Child;
+        if (sigaction(SIGINT, &new_act, NULL) < 0) {
+            perror ("Sigaction ERROR\n");
+            exit(3);
+        }
         close(mypipe[1]);
         read(mypipe[0], buff_c, 100);
         close(mypipe[0]);
-        args = malloc((100)*sizeof(char*));
         first_arg =  malloc(100*sizeof(char));
-        count = get_args(buff_c, first_arg, args);
-        for (i = 0; i < count; ++i){
-            printf("Args[0]:%s\n",  args[i]);
-            
-         } 
-        // args = realloc(args,(count)*sizeof(char*));
+        
+        get_args(buff_c, first_arg, args);
+        //printf("%f\n", sizeof(args));
+        sigsetjmp(obl,1);
+        sleep(3);
         execv(first_arg, args);
     }
     else{
         //Parent;
+        sigsetjmp(obl,1);	
         printf("Input command with arguments:\n");
         close(mypipe[0]);
         fgets(buff_p, 100, stdin);
-        sigsetjmp(obl, 1);
+        sigsetjmp(obl,1);
         write(mypipe[1], buff_p, strlen(buff_p));
         close(mypipe[1]);
         wait(0);
 
     }
-
+    //sigsetjmp(obl,1);
+    //printf("lol");
+    
     return 0;
     
 }
